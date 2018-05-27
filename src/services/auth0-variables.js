3bdb5c9d6f6c161c59c8ab27ca8427cf1572be42
My Nguyen <my@vnguyen.net>
2018-05-27 13:45:09 -0500

    Add authentication service

---
+++ src/services/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  clientId: process.env.AUTH0_CLIENT_ID,
+  domain: process.env.AUTH0_DOMAIN,
+  callbackUrl: process.event.AUTH0_CALLBACK,
+  apiUrl: "API_IDENTIFIER"
+};
